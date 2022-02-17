27ba2ce9cd89b8bb9cb4437cdb06d8b3757d175a
Henrique Pina <henrique.marques.pina@gmail.com>
2022-02-16 23:53:23 -0300

    todas as perguntas respondidas no console

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const textoCompleto = '         <html><head><title>Gulliver Traveller - Roteiros
 
 // listando as cidade
 const listaCidades = textoCompleto.split('*');
-console.log(listaCidades);
 const cidades = [];
 let indexCidades = 1;
 do{
@@ -15,7 +14,6 @@ console.log('As cidades avaliadas por Gulliver em seu antigo blog são: ',cidade
 
 // listando todos os roteiros
 const listaRoteiros = textoCompleto.split('#');
-console.log(listaRoteiros);
 let indexRoteiros = 1;
 const roteiros = [];
 do{
@@ -23,7 +21,6 @@ do{
     indexRoteiros++
 }
 while(indexRoteiros < listaRoteiros.length)
-    console.log(roteiros);
 
  // listando roteiros A de cada cidade
 const roteirosA = [];
@@ -34,37 +31,88 @@ do{
 }
 while(indexListaRoteiros < roteiros.length)
 console.log('Na cidade de ',cidades[0],' temos:',roteirosA[0],'. Na cidade de ',cidades[1],' temos:',roteirosA[1],'. E na cidade de',cidades[2],' temos:',roteirosA[2]);
-console.log(roteirosA);
-
 
 // Quantos locais são citados no roteiro A de cada cidade.
 let indexLocais =0;
-
 const listaLocaisCidade1 = roteirosA[0].split('<br>');
 const listaLocaisCidade2 = roteirosA[1].split('<br>');
 const listaLocaisCidade3 = roteirosA[2].split('<br>');
-console.log(listaLocaisCidade1);
-console.log(listaLocaisCidade2);
-console.log(listaLocaisCidade3);
 const locaisRoteirosAcidade1 = listaLocaisCidade1[1].split(';');
 const locaisRoteirosAcidade2 = listaLocaisCidade2[1].split(';');
 const locaisRoteirosAcidade3 = listaLocaisCidade3[1].split(';');
 
 console.log('No roteiro A da cidade',cidades[0], 'temos',locaisRoteirosAcidade1.length, 'locais. No roteiro A da cidade',cidades[1], 'temos',locaisRoteirosAcidade2.length, 'locais. E no roteiro A da cidade',cidades[2], 'temos',locaisRoteirosAcidade3.length, 'locais');
 
+// alert('No roteiro A da cidade '+cidades[0]+' temos '+locaisRoteirosAcidade1.length+' locais. No roteiro A da cidade '+cidades[1]+ ' temos '+locaisRoteirosAcidade2.length+' locais. E no roteiro A da cidade '+cidades[2]+' temos '+locaisRoteirosAcidade3.length+' locais.');
 
 // O nome dos pontos turísticos localizados no bairro Centro da cidade de São Paulo. (‘Região: ‘,‘Centro’)
-console.log(cidades);
-indexCidades = 0;
-// let resultado = '';
-do{
-    let resultado = cidades[indexCidades].includes('São Paulo');
-    if(resultado === true){
-        console.log('Acerto Mizeravi');
-    }
-    else{
-        console.log('Burooo');
-        indexCidades++
-    }
+// encontrando a cidade solicitada
+let cidadeSolicitada = 'São Paulo';
+let localSolicitado = 'Centro';
+let resultado2 = '';
+let resultado = '';
+let indexTeste = 0;
+if (cidades.indexOf(cidadeSolicitada) == -1){
+    console.log('Cidade ',cidadeSolicitada,' não faz parte das cidades avaliadas por Gulliver em seu antigo blog');
 }
-while (indexCidades < cidades.length)
+else{
+do{
+    resultado = cidades[indexTeste].includes(cidadeSolicitada);
+    resultado2 = indexTeste;
+    indexTeste++
+}
+while(resultado != true)
+
+// separando os elementos da cidade solicitada
+const centro = listaCidades[resultado2+2];
+const centro2 = centro.split('<br>');
+// encontrando o indez para listar as atrações do local desejado
+let resultado4 = '';
+let resultado3 = '';
+let indexTeste2 = 0;
+do{
+    resultado3 = centro2[indexTeste2].includes(localSolicitado);
+    resultado4 = indexTeste2;
+    indexTeste2++
+}
+while(resultado3 != true)
+console.log('Os pontos turísticos localizados no bairro ',localSolicitado,' da cidade de ',cidadeSolicitada,' são: ',centro2[resultado4+1]);
+}
+
+// O nome dos pontos turísticos localizados no bairro Downtown na cidade de Los Angeles.
+cidadeSolicitada = 'Los Angeles';
+localSolicitado = 'Downtown';
+resultado2 = '';
+resultado = '';
+indexTeste = 0;
+if (cidades.indexOf(cidadeSolicitada) == -1){
+    console.log('Cidade ',cidadeSolicitada,' não faz parte das cidades avaliadas por Gulliver em seu antigo blog');
+}
+else{
+
+    do{
+        resultado = cidades[indexTeste].includes(cidadeSolicitada);
+        resultado2 = indexTeste;
+        indexTeste++
+    }
+    while(resultado != true)
+    console.log(resultado2);
+    
+    // separando os elementos da cidade solicitada
+    centro = listaCidades[resultado2+2];
+    console.log(centro);
+    centro2 = centro.split('<br>');
+    console.log(centro2);
+    // encontrando o indez para listar as atrações do local desejado
+    resultado4 = '';
+    resultado3 = '';
+    indexTeste2 = 0;
+    do{
+        resultado3 = centro2[indexTeste2].includes(localSolicitado);
+        resultado4 = indexTeste2;
+        indexTeste2++
+    }
+    while(resultado3 != true)
+    console.log(resultado4);
+    console.log('Os pontos turísticos localizados no bairro ',localSolicitado,' da cidade de ',cidadeSolicitada,' são: ',centro2[resultado4+1]);
+}   
